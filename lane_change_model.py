4d91ccc1145b003b16e52f515e22148997c03300
leihuang2 <1997120908hL.3>
2023-06-21 18:16:58 +0800

    注意力参数修改

--- lane_change_model.py
+++ lane_change_model.py
@@ -17,7 +17,7 @@ class LaneChangeModel(nn.Module):
         self.surrounding_vehicle_encoding = nn.Sequential(nn.Linear(self.surrounding_feature_size, embedding_size), nn.BatchNorm1d(embedding_size), nn.LeakyReLU())
         self.lstm_surrounding = nn.LSTM(self.embedding_size, lstm_hidden_size, num_stacked_layers, batch_first=True)
         self.lstm_target = nn.LSTM(self.embedding_size, lstm_hidden_size, num_stacked_layers, batch_first=True)
-        self.att = nn.MultiheadAttention(self.lstm_hidden_size, 16, 0.5, batch_first=True)
+        self.att = nn.MultiheadAttention(self.lstm_hidden_size, 32, 0.5, batch_first=True)
         self.fc1 = nn.Sequential(nn.Linear(lstm_hidden_size, fc_size[0]), nn.BatchNorm1d(fc_size[0]), nn.LeakyReLU())
         self.fc2 = nn.Sequential(nn.Linear(fc_size[0], fc_size[1]), nn.BatchNorm1d(fc_size[1]), nn.LeakyReLU())
         self.fc3 = nn.Sequential(nn.Linear(fc_size[1], fc_size[2]), nn.BatchNorm1d(fc_size[2]), nn.LeakyReLU())
@@ -37,6 +37,7 @@ class LaneChangeModel(nn.Module):
         h0 = torch.zeros(self.num_stacked_layers, batch_size, self.lstm_hidden_size).to(device)
         c0 = torch.zeros(self.num_stacked_layers, batch_size, self.lstm_hidden_size).to(device)
         lstm_target_out, _ = self.lstm_target(target_vehicle_vector, (h0, c0))
+        lstm_target_out = self.activation(lstm_target_out)
         lstm_target_out = lstm_target_out[:, -1, :].view(batch_size, 1, self.lstm_hidden_size)
 
         # surrounding vehicle embedding
@@ -49,10 +50,11 @@ class LaneChangeModel(nn.Module):
         c0 = torch.zeros(self.num_stacked_layers, batch_size * 5, self.lstm_hidden_size).to(device)
         surrounding_vehicle_vector = surrounding_vehicle_vector.view(batch_size * 5, self.time_horizon, self.embedding_size)
         lstm_surrounding_out, _ = self.lstm_surrounding(surrounding_vehicle_vector, (h0, c0))
+        lstm_surrounding_out = self.activation(lstm_surrounding_out)
         lstm_surrounding_out = lstm_surrounding_out[:, -1, :]
         lstm_surrounding_out = lstm_surrounding_out.view(batch_size, 5, self.lstm_hidden_size)
 
-        # ATT (waiting to do)
+        # ATT
         q = lstm_target_out
         k, v = lstm_surrounding_out, lstm_surrounding_out
         att_output, _ = self.att(q, k, v)
